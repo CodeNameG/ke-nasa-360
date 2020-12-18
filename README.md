fcd02e23318e9bdf531852705f7778d2e740a113
CodeNameG <bonefasioglent@live.com>
2020-12-18 13:44:49 +0700

    forst commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#main-kenasa-360
---
+++ data.js
@@ -0,0 +1,453 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-resepsionis",
+      "name": "resepsionis",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "yaw": 0.44573023974009907,
+        "pitch": 0.432929340118136,
+        "fov": 1.362234561578609
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5038395717402899,
+          "pitch": 0.3501874448305369,
+          "rotation": 4.71238898038469,
+          "target": "1-ruangan-mirip-gudang"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4949517372498704,
+          "pitch": 0.5045652046646083,
+          "title": "resepsionis",
+          "text": "kayaknya sih bukan resepsionis, tapi kita anggep aja gitu<br>"
+        },
+        {
+          "yaw": -0.27564014607418486,
+          "pitch": 0.17778071925212302,
+          "title": "om om mau nanya<br>",
+          "text": "si om mau nanya ke resepsionis, tapi jaraknya jauh beud dah, orangnya di mana, dia bedirinya di mana, majuan napa sih?<br>"
+        },
+        {
+          "yaw": -0.7248676907082316,
+          "pitch": 0.3204273888551281,
+          "title": "jurus bayangan<br>",
+          "text": "ada Naruto lagi nyobain jurus bayangan di NASA<br>"
+        },
+        {
+          "yaw": 0.9386636824617902,
+          "pitch": -0.198041442515418,
+          "title": "Layar gede<br>",
+          "text": "gatau ini layar buat CCTV atau buat apaan, abisnya ada kaya layar excel gitu, yang pasti ini layarnya gede<br>"
+        },
+        {
+          "yaw": -1.7375338438170331,
+          "pitch": 0.5503429863089906,
+          "title": "ember atau biasa disebut tong atau gentong<br>",
+          "text": "terbuat dari plastik kayanya, dan berwarna biru<br>"
+        },
+        {
+          "yaw": -1.0026146452504605,
+          "pitch": 0.29649456462983714,
+          "title": "denah ruangan<br>",
+          "text": "mungkin bisa diapalin biar ga kesasar<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-ruangan-mirip-gudang",
+      "name": "ruangan mirip gudang",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6983206986592272,
+          "pitch": 0.1296536435118334,
+          "rotation": 4.71238898038469,
+          "target": "2-ruangan-besar"
+        },
+        {
+          "yaw": 3.1336408244607252,
+          "pitch": 0.09159495255292782,
+          "rotation": 0,
+          "target": "0-resepsionis"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-ruangan-besar",
+      "name": "ruangan besar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.24728286094401142,
+          "pitch": 0.33729857613595726,
+          "rotation": 0,
+          "target": "3-mesin-raffaello"
+        },
+        {
+          "yaw": 3.0067394989647216,
+          "pitch": 0.07623710316553556,
+          "rotation": 0,
+          "target": "1-ruangan-mirip-gudang"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.10165598005506027,
+          "pitch": -0.17648743797020572,
+          "title": "Mesin gede beud dah sumpah<br>",
+          "text": "wah mesin apa ini? sepertinya menarik, ayo kita mendekat<br>"
+        },
+        {
+          "yaw": -0.7415722646525467,
+          "pitch": 0.5395724131193731,
+          "title": "pesan dari NASA untuk para pekerja, kayaknya<br>",
+          "text": "Ternyata gaya design NASA mirip kaya design pabrik pabrikan di endonesa gitu ya, lucuk<br>"
+        }
+      ]
+    },
+    {
+      "id": "3-mesin-raffaello",
+      "name": "mesin raffaello",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "yaw": 1.5898380302024586,
+        "pitch": -0.048535124381874795,
+        "fov": 1.362234561578609
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1195922637314624,
+          "pitch": 0.15486163900390437,
+          "rotation": 0,
+          "target": "4-sebelah-mesin-raffaello"
+        },
+        {
+          "yaw": -2.681503923174839,
+          "pitch": 0.38109271860095006,
+          "rotation": 0.7853981633974483,
+          "target": "2-ruangan-besar"
+        },
+        {
+          "yaw": -0.020327559760076497,
+          "pitch": 0.08926371157941482,
+          "rotation": 0,
+          "target": "5-ujung-ruangan"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8027453296072178,
+          "pitch": -0.33853426657227814,
+          "title": "mesin raffaello<br>",
+          "text": "sebenernya nama mesinnya bukan raffaello, tapi karena ada tulisan raffaellonya, maka kita sebut saja nama mesin ini rafaello<br>"
+        },
+        {
+          "yaw": 0.8054347466051226,
+          "pitch": -0.420874435991232,
+          "title": "bendera amerika<br>",
+          "text": "bendera amerika, karna ini NASA, coba kalo ini LAPAN, pasti bendera indonesia yang di pajang<br>"
+        },
+        {
+          "yaw": -0.8226938016102388,
+          "pitch": -0.2591298070882111,
+          "title": "banner NASA<br>",
+          "text": "kenapa dinamakan NASA? karena kalau Dina makan NASI, nanti kenyang<br>"
+        }
+      ]
+    },
+    {
+      "id": "4-sebelah-mesin-raffaello",
+      "name": "sebelah mesin raffaello",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "yaw": -2.9394536852674467,
+        "pitch": 0.12324194482607886,
+        "fov": 1.362234561578609
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20684536310827362,
+          "pitch": 0.16101631790856885,
+          "rotation": 0.7853981633974483,
+          "target": "3-mesin-raffaello"
+        },
+        {
+          "yaw": -3.0125512429317887,
+          "pitch": 0.1447735102167833,
+          "rotation": 0,
+          "target": "6-ujung-sana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.7113725195008094,
+          "pitch": 0.015481928181863225,
+          "title": "tangga",
+          "text": "ini tangga buat naik, buat turun juga bisa, kalau lagi di atas tapinya<br>"
+        }
+      ]
+    },
+    {
+      "id": "5-ujung-ruangan",
+      "name": "ujung ruangan",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1333603370303855,
+          "pitch": 0.10578937869954608,
+          "rotation": 0,
+          "target": "3-mesin-raffaello"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-ujung-sana",
+      "name": "ujung sana",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "yaw": -2.8040230118722604,
+        "pitch": 0.2146036832994227,
+        "fov": 1.362234561578609
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0387701196038552,
+          "pitch": 0.16405815340753982,
+          "rotation": 0,
+          "target": "7-wah-ternyata-ada-yang-lebih-ujung-lagi"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0609448867051015,
+          "pitch": 0.026743146722553846,
+          "title": "wah apa tuch?<br>",
+          "text": "wah ada apaan tuh? banyak kotretan, seperti papan tulis<br>"
+        }
+      ]
+    },
+    {
+      "id": "7-wah-ternyata-ada-yang-lebih-ujung-lagi",
+      "name": "wah ternyata ada yang lebih ujung lagi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "yaw": -1.7962022272790623,
+        "pitch": 0.08024121390119987,
+        "fov": 1.362234561578609
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4943048682088964,
+          "pitch": 0.20422827411974964,
+          "rotation": 0,
+          "target": "6-ujung-sana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.5511190909765737,
+          "pitch": 0.04391295876595258,
+          "title": "ternyata bukan papan tulis<br>",
+          "text": "bukan papan tulis, tapi banyak tulisan, mungkin namanya tembok tulis<br>"
+        },
+        {
+          "yaw": 2.6087361934117093,
+          "pitch": 0.07455513293060889,
+          "title": "pintu exit<br>",
+          "text": "tulisannya exit, bisa keluar ruangan dari sini kalo darurat, tapi ga ketemu foto exitnya, jadi pake info gini aja ya biar ketauan kalo bisa exit dari sini<br>"
+        },
+        {
+          "yaw": 2.6842165247703402,
+          "pitch": -0.34781477378775705,
+          "title": "tulisan exit yang tinggi<br>",
+          "text": "tulisan exit, tapi nempelnya tinggi banget, mungkin biar bisa keliatan dari jauh, atau biar bisa diliat sama orang tinggi jadi dia gausah nunduk buat ngeliat yang tulisan exit di bawah<br>"
+        },
+        {
+          "yaw": 0.21788499040418685,
+          "pitch": 0.5831542516563939,
+          "title": "bangku<br>",
+          "text": "buat duduk<br>"
+        }
+      ]
+    }
+  ],
+  "name": "latihan bikin 360 dari foto nemu google",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
